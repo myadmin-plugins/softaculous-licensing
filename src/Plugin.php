0bf769481a28e36cec6b7828c430f4bb42eeb60a
Joe Huss <detain@interserver.net>
2024-12-05 02:37:50 -0500

    update to plugins

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,7 +3,7 @@
  * Softaculous Related Functionality
  *
  * @author Joe Huss <detain@interserver.net>
- * @copyright 2019
+ * @copyright 2025
  * @package MyAdmin-Softaculous-Licensing
  * @category Licenses
  */
